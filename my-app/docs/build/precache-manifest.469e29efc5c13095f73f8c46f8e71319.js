fd6f9008bd97e016a5e5dbd9eac0760837ea8def
mablancoalvarez <mablancoalvarez@gmail.com>
2020-04-07 10:18:13 +0200

    add final version

---
+++ my-app/docs/build/precache-manifest.469e29efc5c13095f73f8c46f8e71319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1750dbdddeac68e101c243fd60d0e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9975946fea99053c57ee",
+    "url": "./static/css/main.82d0306b.chunk.css"
+  },
+  {
+    "revision": "73c53154cac7b9d8afdc",
+    "url": "./static/js/2.db6305ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db6305ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9975946fea99053c57ee",
+    "url": "./static/js/main.99e728b3.chunk.js"
+  },
+  {
+    "revision": "29f1bb0b68c6f5cb7210",
+    "url": "./static/js/runtime-main.66d39b23.js"
+  }
+]);
