4513d1462c17f9d11999e37d30c850848a817d3a
mablancoalvarez <mablancoalvarez@gmail.com>
2020-04-07 10:36:02 +0200

    final version

---
+++ docs/build/precache-manifest.c16db7a58f0e9656f41d55c0a3e3673b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93bb42c017b7d126c0917f2baa239cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49d3b130661ac97cea02",
+    "url": "./static/css/main.82d0306b.chunk.css"
+  },
+  {
+    "revision": "73c53154cac7b9d8afdc",
+    "url": "./static/js/2.db6305ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db6305ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d3b130661ac97cea02",
+    "url": "./static/js/main.d17a76dc.chunk.js"
+  },
+  {
+    "revision": "29f1bb0b68c6f5cb7210",
+    "url": "./static/js/runtime-main.66d39b23.js"
+  }
+]);
